22e1907ccd8c82a262df6a77af7ec9d5e7fa22d0
Samuel Colvin <s@muelcolvin.com>
2020-01-09 15:40:31 +0000

    tweak binary build

--- install-rust.sh
+++ install-rust.sh
@@ -6,7 +6,9 @@ pip install -U pip setuptools setuptools-rust cibuildwheel
 if [ ! -d ~/rust-installer ]; then
     mkdir ~/rust-installer
     curl -sL https://static.rust-lang.org/rustup.sh -o ~/rust-installer/rustup.sh
-    sh ~/rust-installer/rustup.sh -y
+    sh ~/rust-installer/rustup.sh --default-toolchain nightly -y
     source $HOME/.cargo/env
     rustup default nightly
+    echo "set rust to nightly"
+    rustc --version
 fi
