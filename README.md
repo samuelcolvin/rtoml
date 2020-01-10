e16c1ce2f2cdfb8fd7885e7e2772e6b314be69a4
Samuel Colvin <s@muelcolvin.com>
2020-01-10 17:40:06 +0000

    uiri/toml and tomlkit benchmarks

--- README.md
+++ README.md
@@ -8,9 +8,14 @@
 
 A better TOML library for python implemented in rust.
 
+rtoml is based on the widely used and very stable [toml-rs](https://github.com/alexcrichton/toml-rs) library,
+it passes all the [standard toml tests](https://github.com/BurntSushi/toml-test) as well as having 100% coverage on
+python code.
+
 ## Install
 
-Requires linux and `python>=3.7`.
+Requires `python>=3.7`, pypi includes binaries for linux, to install on windows or mac you'll need rust nightly and
+[`setuptools-rust`](https://github.com/PyO3/setuptools-rust) installed.
 
 ```bash
 pip install rtoml
--- benchmarks/run.py
+++ benchmarks/run.py
@@ -2,28 +2,42 @@
 import timeit
 from pathlib import Path
 
+import toml
 import tomlkit
 
 import rtoml
 
-toml = (Path(__file__).parent / 'data.toml').read_text()
+print(f'rtoml version: {rtoml.VERSION}')
+toml_str = (Path(__file__).parent / 'data.toml').read_text()
 
 
 def rtoml_load():
-    return rtoml.loads(toml)
+    return rtoml.loads(toml_str)
+
+
+def uiri_toml_load():
+    return toml.loads(toml_str)
 
 
 def tomlkit_load():
-    return tomlkit.parse(toml)
+    return tomlkit.parse(toml_str)
 
 
-rtoml_data = rtoml_load()
-debug(rtoml_data)
-debug(list(rtoml_data.keys()))
-# assert rtoml_data == tomlkit_load()
+def test_matching_output():
+    rtoml_data = rtoml_load()
+    # debug(rtoml_data)
+    assert rtoml_data == uiri_toml_load()
+    assert rtoml_data == tomlkit_load()
 
 
-steps = 100
+if __name__ == '__main__':
+    steps = 100
 
-print(timeit.timeit(rtoml_load, number=steps))
-# print(timeit.timeit(pyd_func, number=100000))
+    rtoml_time = timeit.timeit(rtoml_load, number=steps)
+    print(f'rtoml:      {rtoml_time / steps * 1000:0.3f} ms/parse')
+
+    toml_time = timeit.timeit(uiri_toml_load, number=steps)
+    print(f'uiri/toml:  {toml_time / steps * 1000:0.3f} ms/parse ({toml_time / rtoml_time:0.2f} X slower)')
+
+    tomlkit_time = timeit.timeit(tomlkit_load, number=steps)
+    print(f'tomlkit:    {tomlkit_time / steps * 1000:0.3f} ms/parse ({tomlkit_time / rtoml_time:0.2f} X slower)')
