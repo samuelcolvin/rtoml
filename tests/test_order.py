74446f1a3de6317c2345673e72fb16f2aaeab8f6
Manfred Lotz <manfred@ctan.org>
2021-03-07 21:44:31 +0100

    changes to make lint happy

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,4 +1,3 @@
-
 import pytest
 
 import rtoml
@@ -9,28 +8,29 @@ the stringified TOML dictionary is in the same sequence
 as the original TOML string.
 """
 
+
 @pytest.mark.parametrize(
     'input_toml,output_toml',
     [
-        ("""
-            something = true
-            lion = 'aslan'
-    """,
-        "{'something': True, 'lion': 'aslan'}"
+        (
+            """
+something = true
+lion = 'aslan'
+""",
+            "{'something': True, 'lion': 'aslan'}",
         ),
-        ("""
-            [section]
-            z = "last"
-            a = "first"
-            [default]
-            dir = "/home"
-            beta = true
-    """,
-        "{'section': {'z': 'last', 'a': 'first'}, 'default': {'dir': '/home', 'beta': True}}"
+        (
+            """
+[section]
+z = "last"
+a = "first"
+[default]
+dir = "/home"
+beta = true
+""",
+            "{'section': {'z': 'last', 'a': 'first'}, 'default': {'dir': '/home', 'beta': True}}",
         ),
-]
+    ],
 )
-
-
 def test_load(input_toml, output_toml):
     assert str(rtoml.load(input_toml)) == output_toml
