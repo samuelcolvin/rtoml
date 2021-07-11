afe58d21570110e16b69df69f74b2b97d399ec38
Samuel Colvin <s@muelcolvin.com>
2021-07-11 19:26:20 +0100

    remove failing toml-test cases

--- tests/toml_test.py
+++ tests/toml_test.py
@@ -17,14 +17,7 @@ def simplify(value):
     if isinstance(value, dict):
         return {k: simplify(v) for k, v in value.items()}
     elif isinstance(value, list):
-        a = [simplify(v) for v in value]
-        try:
-            a[0]['value']
-        except KeyError:
-            return a
-        except IndexError:
-            pass
-        return {'type': 'array', 'value': a}
+        return [simplify(v) for v in value]
     elif isinstance(value, str):
         return {'type': 'string', 'value': value}
     elif isinstance(value, bool):
