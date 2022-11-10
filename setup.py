4d3075d29bf82c7fb2fd33b8ba76843f9c2748d6
Samuel Colvin <s@muelcolvin.com>
2022-11-10 11:52:49 +0000

    switch to maturin for build

--- setup.py
+++ setup.py
@@ -1,54 +1,21 @@
-import re
-from pathlib import Path
+import sys
 
-from setuptools import setup
-from setuptools_rust import Binding, RustExtension
-
-description = 'A better TOML library for python implemented in rust.'
-
-THIS_DIR = Path(__file__).resolve().parent
-try:
-    long_description = (THIS_DIR / 'README.md').read_text()
-except FileNotFoundError:
-    long_description = description
-
-# VERSION is set in Cargo.toml
-cargo = Path(THIS_DIR / 'Cargo.toml').read_text()
-VERSION = re.search('version *= *"(.*?)"', cargo).group(1)
-
-setup(
-    name='rtoml',
-    version=VERSION,
-    description=description,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Samuel Colvin',
-    author_email='s@muelcolvin.com',
-    url='https://github.com/samuelcolvin/rtoml',
-    license='MIT',
-    package_data={'rtoml': ['py.typed']},
-    python_requires='>=3.7',
-    rust_extensions=[RustExtension('rtoml._rtoml', binding=Binding.PyO3)],
-    packages=['rtoml'],
-    zip_safe=False,
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX :: Linux',
-        'Environment :: Console',
-        'Environment :: MacOS X',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Internet',
-    ],
+sys.stderr.write(
+    """
+===============================
+Unsupported installation method
+===============================
+rtoml no longer supports installation with `python setup.py install`.
+Please use `python -m pip install .` instead.
+"""
 )
+sys.exit(1)
+
+
+# The below code will never execute, however GitHub is particularly
+# picky about where it finds Python packaging metadata.
+# See: https://github.com/github/feedback/discussions/6456
+#
+# To be removed once GitHub catches up.
+
+setup(name='rtoml')
